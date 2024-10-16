402fdce7ef81bbb3721a297c623dd6b2511e3eec
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-10-16 16:38:08 +0200

    aggiunti i metodi

--- formComponent.js
+++ formComponent.js
@@ -1,3 +1,19 @@
 const generateReservationForm = (parentElement) => {
+    let configuration;
+    let callback;
 
+    return {
+        build: (inputConfiguration) => {
+            configuration = inputConfiguration;
+        },
+        onsubmit: (inputCallback) => {
+
+        },
+        render: () => {
+            let html = '<form id="reservationForm" class="container"><label>Data</label><input type="date" id="dateInput">';
+            
+            html += Object.keys(configuration).map(e => '<label>' + e.substring(0, 1).toUpperCase() + e.substring(1, e.length) + '</label>' + '<input type="number" id="' + e + '">').join("");
+            console.log(html)
+        }
+    };
 };
