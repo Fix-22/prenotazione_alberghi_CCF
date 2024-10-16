d93b72fa5fdc83f268b499d85296ac4695d1d6af
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-10-16 16:41:14 +0200

    finalizzata render

--- formComponent.js
+++ formComponent.js
@@ -12,8 +12,9 @@ const generateReservationForm = (parentElement) => {
         render: () => {
             let html = '<form id="reservationForm" class="container"><label>Data</label><input type="date" id="dateInput">';
             
-            html += Object.keys(configuration).map(e => '<label>' + e.substring(0, 1).toUpperCase() + e.substring(1, e.length) + '</label>' + '<input type="number" id="' + e + '">').join("");
-            console.log(html)
+            html += Object.keys(configuration).map(e => '<label>' + e.substring(0, 1).toUpperCase() + e.substring(1, e.length) + '</label>' + '<input type="number" id="' + e + '">').join("") + "</form>";
+            
+            parentElement.innerHTML = html;
         }
     };
 };
