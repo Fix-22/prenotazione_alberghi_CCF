19c2ecb9594ba14ce75148491472589b3d5ca3ba
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-10-16 16:41:55 +0200

    aggiunto il componente del form

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+const formContainer = document.getElementById("formContainer");
+
 const conf = {
     "singola": 10,
     "doppia": 5,
     "suite": 3
 }
+
+const reservationForm = generateReservationForm(formContainer);
+reservationForm.build(conf);
+reservationForm.render();
