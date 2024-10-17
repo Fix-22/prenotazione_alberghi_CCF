28919f86860fe46e8e604c52d3464ac41afebc75
Andrea Falcone <trustypixel@IdeaPad-3-15IML05-0980413a.localdomain>
2024-10-17 10:49:45 +0000

    Aggiunto Table

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const formContainer = document.getElementById("formContainer");
+const tableContainer = document.getElementById("tableContainer");
 
 const conf = {
     "singola": 10,
@@ -6,7 +7,12 @@ const conf = {
     "suite": 3
 }
 
+
 const reservationForm = generateReservationForm(formContainer);
 reservationForm.build(conf);
 reservationForm.onsubmit(reservation => console.log(reservation));
 reservationForm.render();
+
+const reservationTable = generateReservationTable(tableContainer);
+reservationTable.build(conf);
+reservationTable.render();
