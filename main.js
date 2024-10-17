76bd479fbec3cbc20debd3d2e5a1395525559b50
Andrea Falcone <trustypixel@IdeaPad-3-15IML05-0980413a.localdomain>
2024-10-17 16:57:36 +0000

    Aggiunta Grafica table / Aggiunte variabili di test per la table

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ const conf = {
     "suite": 3
 }
 
+const testData = [
+    {
+        "data": "10/3/2024",
+        "singola": 10,
+        "doppia": 5,
+        "suite": 3
+    },
+    {
+        "data": "23/3/2028",
+        "singola": 10,
+        "doppia": 3,
+        "suite": 2
+    },
+    {
+        "data": "31/12/2020",
+        "singola": 5,
+        "doppia": 2,
+        "suite": 1
+    },
+]
 
 const reservationForm = generateReservationForm(formContainer);
 reservationForm.build(conf);
@@ -15,4 +35,5 @@ reservationForm.render();
 
 const reservationTable = generateReservationTable(tableContainer);
 reservationTable.build(conf);
+reservationTable.updateData(testData);
 reservationTable.render();
--- tableComponent.js
+++ tableComponent.js
@@ -1,3 +1,36 @@
 const generateReservationTable = (parentElement) => {
+    let configuration;
     let data = [];
+
+    return {
+        build: (inputConfiguration) => {
+            configuration = inputConfiguration;
+        },
+        updateData: (newData) => {
+            data = newData;
+        },
+        render: () => {
+            let htmlTable = "<table class='table'><tr>";
+            //Headers
+            let headers = Object.keys(configuration);
+            headers.unshift("data");
+            for (let i = 0; i < headers.length; i++) {
+                htmlTable += "<th>" + headers[i] + "</th>"
+            }
+            htmlTable += "</tr>"
+            //Body
+            data.map((current) => {
+                
+                htmlTable += "<tr>"
+                for (let i = 0; i < headers.length; i++) {
+                    console.log(headers[i]);
+                    console.log(current[headers[i]]);
+                    htmlTable += "<td>" + current[headers[i]]+ "</td>";
+                    console.log(htmlTable);
+                }
+                htmlTable += "</tr>"
+            });
+            parentElement.innerHTML = htmlTable;
+        },
+    }
 };
